98f777c7e0b51262b38f5e5c10db20929bac76e9
XiaoxiaYao <michaelyaonz@gmail.com>
2020-12-30 09:50:21 +0800

    Use mixin to create factory like functions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import Button from './components/Button/button';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
-    <div className="App">
-      <Button>Click</Button>
-    </div>
+    <>
+      <Button btnType={ButtonType.Primary}>Primary</Button>
+      <Button btnType={ButtonType.Danger}>Danger</Button>
+      <Button btnType={ButtonType.Default}>Default</Button>
+      <Button btnType={ButtonType.Link}>Link</Button>
+
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        Small
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large
+      </Button>
+
+      <Button btnType={ButtonType.Default} disabled>
+        Default
+      </Button>
+
+      <Button btnType={ButtonType.Primary} disabled>
+        Disabled Primary
+      </Button>
+
+      <Button btnType={ButtonType.Link} disabled>
+        Disabled Link
+      </Button>
+    </>
   );
 }
 
